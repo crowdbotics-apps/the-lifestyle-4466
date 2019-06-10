14d67441f4fadf5c6ae3bd1ffb982b3c0cb0199e
Crowdbotics <team@crowdbotics.com>
2019-06-10 02:14:08 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.the_lifestyle_4466.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/the_lifestyle_4466/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for the_lifestyle_4466 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_lifestyle_4466.settings")
+
+application = get_wsgi_application()
